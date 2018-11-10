73e45129ad2661b3361ee2fd811e61876eb6bbf4
Giacomo De Liberali <deliberali.giacomo@gmail.com>
2018-11-10 20:15:00 +0100

    Add tox, travis and fix tests

--- monolith/tests/test_runs.py
+++ monolith/tests/test_runs.py
@@ -1,80 +1,69 @@
-import unittest, json
-from flask import request, jsonify
-from monolith.app import create_app
-from monolith.views.runs import _runs
-from flask import Blueprint, render_template
-from monolith.database import db, Run, User, _delete_user
-from monolith.auth import current_user
-from werkzeug import ImmutableMultiDict
+from monolith.app import create_testing_app
+from monolith.database import Run, User, _delete_user
 import random
 
 
-class TestApp(unittest.TestCase):
-
-	#if any of runs were deleted
-	def test_runs1(self):
-		app = create_app()
-		email = 'mock' + str(random.randint(1, 101)) + '@mock.com'
-		password = 'mock'
-		example = User()
-		example.email = email
-		example.set_password(password)
+#if any of runs were deleted
+def test_runs1(db_instance):
+	email = 'mock' + str(random.randint(1, 101)) + '@mock.com'
+	password = 'mock'
+	example = User()
+	example.email = email
+	example.set_password(password)
 
 
-		runs_id =['1', '2', '3', '4', '5', '6', '7']
+	runs_id =['1', '2', '3', '4', '5', '6', '7']
 
-		with app.app_context():
-			db.session.add(example)
+	db_instance.session.add(example)
 
-			for i in runs_id:
-				run = Run()
-				run.runner = example
-				run.strava_id = i
-				db.session.add(run)
-			
-			db.session.commit()
-			user_id = example.get_id()
-
-
-			previous_run = db.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id<7).order_by(Run.id.desc()).first()
-			self.assertEqual(previous_run.id, 6)
-
-			q = db.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id == 4)
-			q.delete(synchronize_session=False)
-			db.session.commit()
-			previous_run = db.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id<5).order_by(Run.id.desc()).first()
-			self.assertEqual(previous_run.id, 3)
-
-			_delete_user(example)
-            
+	for i in runs_id:
+		run = Run()
+		run.runner = example
+		run.strava_id = i
+		db_instance.session.add(run)
+	
+	db_instance.session.commit()
+	user_id = example.get_id()
 
 
-	#if given id is larger than all run's  id
-	def test_runs2(self):
-		app = create_app()
-		email = 'mock' + str(random.randint(1, 101)) + '@mock.com'
-		password = 'mock'
-		example = User()
-		example.email = email
-		example.set_password(password)
+	previous_run = db_instance.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id<7).order_by(Run.id.desc()).first()
+	assert previous_run.id == 6
+
+	q = db_instance.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id == 4)
+	q.delete(synchronize_session=False)
+	db_instance.session.commit()
+	previous_run = db_instance.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id<5).order_by(Run.id.desc()).first()
+	assert previous_run.id == 3
+
+	_delete_user(example)
+		
 
 
-		runs_id =['1', '2', '3', '4', '5', '6', '7']
+#if given id is larger than all run's  id
+def test_runs2(db_instance):
+	email = 'mock' + str(random.randint(1, 101)) + '@mock.com'
+	password = 'mock'
+	example = User()
+	example.email = email
+	example.set_password(password)
 
-		with app.app_context():
-			db.session.add(example)
 
-			for i in runs_id:
-				run = Run()
-				run.runner = example
-				run.strava_id = i
-				db.session.add(run)
-			
-			db.session.commit()
-			user_id = example.get_id()
-			runId=150
+	runs_id =['1', '2', '3', '4', '5', '6', '7']
 
-			run = db.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id == runId).first()
-			self.assertEqual(run, None)
+	
+	db_instance.session.add(example)
 
-			_delete_user(example)
+	for i in runs_id:
+		run = Run()
+		run.runner = example
+		run.strava_id = i
+		db_instance.session.add(run)
+	
+	db_instance.session.commit()
+	user_id = example.get_id()
+	runId=150
+
+	run = db_instance.session.query(Run).filter(Run.runner_id == example.get_id(), Run.id == runId).first()
+	assert run == None
+
+	_delete_user(example)
